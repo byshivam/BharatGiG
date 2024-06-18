28b1006adc6fce0b0b80346c67c0310e57d89902
SHIVAM KUMAR <60932285+byshivam@users.noreply.github.com>
2024-06-18 17:48:15 +0530

    Update projects.js

--- task_1/pages/projects.js
+++ task_1/pages/projects.js
@@ -6,7 +6,7 @@ export default function project(){
         <>
         <section className="p-4 h-full">
             <div className="flex  justify-center item-center">
-            <Cards title='BharatGiG' imgSrc='/bharatGIG.png' desc='This Project contains the projects and tasks I completed during my internship at #BharatGiG. The internship focused on enhancing my skills in web development' URL='#'/>
+            <Cards title='BharatGiG' imgSrc='/bharatGIG.png' desc='This Project contains the projects and tasks I completed during my internship at #BharatGiG. The internship focused on enhancing my skills in web development' URL='https://github.com/byshivam/BharatGiG'/>
             <Cards title='AnimeZ' imgSrc='/animez.png' desc='Animez is your go-to anime website, offering an extensive database of characters, series, movies, and more. Discover and explore the vast world of anime all in one place with Animez.' URL='#'/>
             <Cards title='BY.SHIVAM' imgSrc='/byshivam.png' desc='“BY.SHIVAM Rebooted" on YouTube showcases my music and video projects, blending creativity, technology, and artistic skills.' URL='https://github.com/byshivam/BharatGiG'/>
             </div>
