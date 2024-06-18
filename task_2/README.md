c3109b47fd3913bfbb2b33885b4b6a53c33eed71
SHIVAM KUMAR <60932285+byshivam@users.noreply.github.com>
2024-06-18 20:37:20 +0530

    Added Calculator

---
+++ task_2/README.md
@@ -0,0 +1,3 @@
+# TASK 2
+### Building a Calculator, via HTML,CSS, JS
+Develop a Calculator app that performs basic math operations Interns will create a web-based calculator application using HTML,CSS, and Javascript to Perform arithmetic Calculations such as addition, subtraction, mutiplication,and division
---
+++ task_2/script.js
@@ -0,0 +1,33 @@
+let string = ""
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.querySelectorAll('.calculator button');
+    Array.from(buttons).forEach((button) =>{
+        button.addEventListener('click',(e) =>{
+            if (e.target.innerHTML === '='){
+                string = eval(parseFloat(string))
+                document.querySelector('input').value = string
+            } 
+
+            else if(e.target.innerHTML === 'AC'){
+                string = "0"
+                document.querySelector('input').value = string
+            }
+
+            else if (e.target.innerHTML === 'C'){
+                string = string.slice(0,-1)
+                document.querySelector('input').value = string
+            }
+
+            else if (e.target.innerHTML === '%'){
+                string = (parseFloat(string) / 100).toString();
+                document.querySelector('input').value = string
+            }
+            
+            else{
+                string =  string + e.target.innerHTML
+                document.querySelector('input').value =string
+            }
+        })
+    })
+});
