2ee1154d1cea4e586da96d388ca6d86138a87a20
SHIVAM KUMAR <60932285+byshivam@users.noreply.github.com>
2024-06-18 17:49:05 +0530

    Update Cards.jsx

--- task_1/src/components/Cards.jsx
+++ task_1/src/components/Cards.jsx
@@ -4,7 +4,7 @@ export default function Cards({ title, imgSrc, desc, URL }) {
     return (
         <div className="bg-gradient-to-b from-dark via-midDark to-black w-60 p-4 text-white m-8 flex flex-col justify-between text-center rounded-xl shadow shadow-white min-h-[20rem]">
             <div className="flex-grow">
-                <Image src={imgSrc} width={200} height={48} className="rounded-xl mx-auto" />
+                <Image src={imgSrc} width={200} height={48} className="rounded-xl mx-auto" alt={title} />
                 <h1 className="font-bold text-2xl mt-4">{title}</h1>
                 <p className="text-[10px] mt-2">{desc}</p>
             </div>
